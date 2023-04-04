f9a76dc76657f6157ec4dc3fb332a1d9012416d6
Profa-Carol-Stone <92635992+Profa-Carol-Stone@users.noreply.github.com>
2023-04-04 20:20:16 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste Carol - Edit <code>src/App.js</code> and save to reload.
+         Fatec
         </p>
         <a
           className="App-link"
